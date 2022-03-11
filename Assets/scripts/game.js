b9b616ce597aaa0007e2d0b1f6230e0ea5d3c40d
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-03-11 18:22:19 +0100

    add mobile version

--- Assets/scripts/game.js
+++ Assets/scripts/game.js
@@ -1,64 +1,129 @@
-const word = "LOL";
-const wordLetters = word.split("");
-const emptyLetters = new Array (word.length)
-console.log(emptyLetters);
-let life = 10;
-const lettreUtilisee=[];
+// fetch('http://game01-dc03.ouiheberg.com:3000/hangman')
+// .then((response) => response.json())
+// .then((data) => {      
+// });
+// Création d'un objet pour récupérer le fichier .json
+// let request = new XMLHttpRequest;
+// request.open('get', 'assets/scripts/words.json', true);
+// request.responseType='json';
+// request.send()
+// request.onload=function(){
+//   let Words = request.response;
+// }
+// const fs =require('fs');
+// fs.open('words.json');
+// import json from './words.json';
+// console.log(json);
+// const word=[''];
 
-function guestWordRender(emptyLetters){
-    const display = [];
-    for (let i=0; i < emptyLetters.lenght; i++){
-        if (emptyLetters[i]){
-            display.push(emptyLetters[i]);
-        }else{
-            display.push('_');
+// Tableaux de mots
+const word = ["chaise","table","fraise","veau","pain","souris","tartine","hamburger","tortue","baguette","ordinateur","donjon","tacos","clavier","lettre","papier","prince","princesse","complainte","marin","tartiflette"] 
+let answer = ''; // variable qui stock les mots du tableau 
+let maxError = 7; // Maximum d'erreur
+let error = 0; // Nombre d'erreur effectuées
+let counterImg = 1; // Compteur d'images
+let guessed = []; //
+let wordStatus = null; // 
+
+// fetch('/Assets/scripts/words.json').then(response => response.json()).then(data=>{
+//   answer = data[0].Words[Math.floor(Math.random()*100)]
+//   console.log(answer);
+// });
+
+        
+        // Fonction qui permet de tirer un mot aléatoirement dans la liste
+        function randomWord() {
+          answer = word[Math.floor(Math.random() * word.length)];
+          console.log(answer);
         }
-    }
-    document.getElementById('emptyLetters').innerHTML=display.join(' ');
+        // Math.floor(Math.random()*data.data.lenght)
+// Création des touches du claviers
+function newButtons() {
+  // création d'une variable pour la création des touches
+  // split sépare les caractères
+  // map permet de retrouver des éléments dans leurs ordre d'insértions
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+      // Permet d'insérer les caractères ci-dessus et de les stylisés via bootstrap
+    `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
-function render(){
-    document.getElementById('life').innerHTML = life;
-    document.getElementById('lettersAlreadyUsed').innerHTML=lettreUtilisee.join(',');
-    guestWordRender(emptyLetters);
-    document.getElementById('selectedLetter').value= "";
+// Fonction motrice du jeu 
+function handleGuess(chosenLetter) {
+  // " ? " = if ; " : " = else
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+  document.getElementById(chosenLetter).style.backgroundColor='#4C4476';
+  // Permet de grisé les cases déjà sélectionner 
+  
+  // Fonction qui permet de vérifié si la lettre encodée fait partie du mot
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+    // sinon, elle rajoute une erreur, upgrade l'image et vérifie si la lettre choisie ne faisait pas partie du mot caché
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    error++;
+    counterImg++;
+    updateError();
+    checkIfGameLost();
+    updateImg();
+  }
 }
 
-function getAllIndex(myWord,mySelectedLetter){
-    const indexes = [];
-    for (let i = 0; i<myWord.lenght; i++){
-        const element = myWord[i];
-        if(element === mySelectedLetter){
-            indexes.push(i);
-        }
-    }
-    return indexes;
+// Fonction qui permet de sauter d'images à chaque erreur
+function updateImg() {
+  document.getElementById('img').src = '/Assets/Images/pendu_' + counterImg + '.png';
+}
+// Fonction qui vérifie si la partie est gagnée
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'You win!';
+  }
 }
 
-function selectedLetter()  {
-    let letter = document.getElementById('selectedLetter').value;
-    letter = letter.trim();
-    const mySelectedLetter = letter[0].toUpperCase();
-    lettreUtilisee.push(mySelectedLetter)
-    const temp = getAllIndex(wordLetters, mySelectedLetter)
-    if(temp.length === 0){
-        life--
-    }else{
-        console.log(temp);
-         for (let i = 0; i < temp.length; i++){
-            emptyLetters[temp[i]] = wordLetters[temp[i]]
-            wordLetters[temp.i] = "";
-            console.log(wordLetters);
-         }
-    }
-    // render();
-    if (life === 0){
-        alert("You lose");
-    }
-    if (wordLetters.every( (el) => el === "" )){
-        alert("You win");
-    }
-};
-onload = function(){
-    render();
+// Fonction qui vérifie si la partie est perdue
+function checkIfGameLost() {
+  if (error === maxError) {
+    document.getElementById('secretWord').innerHTML = 'The answer was: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'You Lose';
+  }
 }
+
+// Fonction qui permet de caché les lettre du mot à déviner
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('secretWord').innerHTML = wordStatus;
+}
+//
+function updateError() {
+  document.getElementById('error').innerHTML = error;
+}
+
+// Fonction qui permet de relancer le jeu
+function reset() {
+  counterImg=1;
+  error = 0;
+  guessed = [];
+  document.getElementById('img').src = '/Assets/Images/pendu_1.png';
+
+  randomWord();
+  guessedWord();
+  updateError();
+  newButtons();
+}
+
+document.getElementById('maxError').innerHTML = maxError;
+
+randomWord();
+newButtons();
+guessedWord();
