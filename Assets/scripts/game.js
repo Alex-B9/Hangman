05d37675ed8200facc055ca699195334ee19f9a1
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-03-12 17:01:38 +0100

    update desktop,tablet and mobile version

--- Assets/scripts/game.js
+++ Assets/scripts/game.js
@@ -18,12 +18,12 @@
 
 // Tableaux de mots
 const word = ["chaise","table","fraise","veau","pain","souris","tartine","hamburger","tortue","baguette","ordinateur","donjon","tacos","clavier","lettre","papier","prince","princesse","complainte","marin","tartiflette"] 
-let answer = ''; // variable qui stock les mots du tableau 
+let answer = ''; // variable qui stockera un mot aléatoire du tableau 'word'
 let maxError = 7; // Maximum d'erreur
 let error = 0; // Nombre d'erreur effectuées
 let counterImg = 1; // Compteur d'images
-let guessed = []; //
-let wordStatus = null; // 
+let guessed = []; // tableau qui récupère answer et le split pour caché les charactères
+let wordStatus = null; // variable qui check le statu du mot pour la conditions de perte/victoire // 'SecretWord'
 
 // fetch('/Assets/scripts/words.json').then(response => response.json()).then(data=>{
 //   answer = data[0].Words[Math.floor(Math.random()*100)]
@@ -55,15 +55,17 @@ function newButtons() {
     `).join('');
 
   document.getElementById('keyboard').innerHTML = buttonsHTML;
+  // affiche les boutons générer sur la page html
 }
 
 // Fonction motrice du jeu 
 function handleGuess(chosenLetter) {
   // " ? " = if ; " : " = else
   guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  // Permet de désactiver les cases déjà sélectionner 
   document.getElementById(chosenLetter).setAttribute('disabled', true);
+  // Permet de changer leurs couleur
   document.getElementById(chosenLetter).style.backgroundColor='#4C4476';
-  // Permet de grisé les cases déjà sélectionner 
   
   // Fonction qui permet de vérifié si la lettre encodée fait partie du mot
   if (answer.indexOf(chosenLetter) >= 0) {
@@ -87,6 +89,9 @@ function updateImg() {
 function checkIfGameWon() {
   if (wordStatus === answer) {
     document.getElementById('keyboard').innerHTML = 'You win!';
+    document.getElementById('keyboard').style.color='#22223B';
+    document.getElementById('keyboard').style.fontSize='35px';
+    //Si oui, éfface le clavier et écrit le message "you win"
   }
 }
 
@@ -95,16 +100,20 @@ function checkIfGameLost() {
   if (error === maxError) {
     document.getElementById('secretWord').innerHTML = 'The answer was: ' + answer;
     document.getElementById('keyboard').innerHTML = 'You Lose';
+    document.getElementById('keyboard').style.color='#22223B';
+    document.getElementById('keyboard').style.fontSize='35px';
+    // Si la partie est perdue, le clavier s'éfface, affiche la réponse + message 'you lose'
   }
 }
 
 // Fonction qui permet de caché les lettre du mot à déviner
+// Prend le mot, sélectionne les lettres et les convertis en charachtère 
 function guessedWord() {
   wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
   document.getElementById('secretWord').innerHTML = wordStatus;
 }
-//
+// Fonction qui affiche le nombre d'erreur
 function updateError() {
   document.getElementById('error').innerHTML = error;
 }
